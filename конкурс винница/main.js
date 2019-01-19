4e1204fef1ffa4e1c7bdd8c4d2fd8421680b9656
artem <45665459+Artemndreevich@users.noreply.github.com>
2019-01-19 18:30:10 +0200

    Update main.js

--- конкурс винница/main.js
+++ конкурс винница/main.js
@@ -8,42 +8,42 @@ $(document).ready(function() {
     $('.plitka_to_text3').hide();
     $('.plitka_to_text4').hide();
 
-    $('.on-plitka-text1').mouseover(function(){
+    $('.on-plitka-text1,#b1block').mouseover(function(){
     
         $('.plitka_to_text1').fadeIn(500);
     });
     
-    $('.on-plitka-text1').mouseout(function(){
+    $('.on-plitka-text1,#b1block').mouseout(function(){
         setTimeout(hidde1,1000);
     });
     function hidde1(){
         $('.plitka_to_text1').fadeOut(100);
     }
-    $('.on-plitka-text2').mouseover(function(){
+    $('.on-plitka-text2,#b2block').mouseover(function(){
         $('.plitka_to_text2').fadeIn(500);
     });
     
-    $('.on-plitka-text2').mouseout(function(){
+    $('.on-plitka-text2,#b2block').mouseout(function(){
         setTimeout(hidde2,1000);
     });
     function hidde2(){
         $('.plitka_to_text2').fadeOut(100);
     }
-    $('.on-plitka-text3').mouseover(function(){
+    $('.on-plitka-text3,#b3block').mouseover(function(){
         $('.plitka_to_text3').fadeIn(500);
     });
     
-    $('.on-plitka-text3').mouseout(function(){
+    $('.on-plitka-text3,#b3block').mouseout(function(){
         setTimeout(hidde3,1000);
     });
     function hidde3(){
         $('.plitka_to_text3').fadeOut(100);
     }
-    $('.on-plitka-text4').mouseover(function(){
+    $('.on-plitka-text4,#b4block').mouseover(function(){
         $('.plitka_to_text4').fadeIn(500);
     });
     
-    $('.on-plitka-text4').mouseout(function(){
+    $('.on-plitka-text4,#b4block').mouseout(function(){
         setTimeout(hidde4,1000);
     });
     function hidde4(){
