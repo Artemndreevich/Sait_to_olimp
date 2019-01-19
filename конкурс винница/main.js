f370d36d2fd0efe19d4796bc290170ec2ff23d91
artem <45665459+Artemndreevich@users.noreply.github.com>
2019-01-19 07:24:03 +0200

    Update main.js

--- конкурс винница/main.js
+++ конкурс винница/main.js
@@ -4,5 +4,9 @@ $(document).ready(function() {
     var menu = $('#menu');
     menu.hide().slideDown(2000);
 
+    ('#b1block').mouseover(function(){
+        ('#b1block').html('<p class="plitka_to_text"> <span class="important_span">rregfd</span>   etggggger</p>');
+
+    });
 
 });
