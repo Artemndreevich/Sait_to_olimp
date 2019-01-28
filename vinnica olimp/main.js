72284073151dd7b48d309785edb4c3747d087df7
Artemndreevich <maestro260205@gmail.com>
2019-01-28 18:28:38 +0200

    all

--- vinnica olimp/main.js
+++ vinnica olimp/main.js
@@ -177,5 +177,195 @@ $('a[href="#p4"]').click(function() {
     }, 400);
     return false;
 });
+
+
+var button = $('#bbb');
+var photo=$('#photo_helper');
+var  textdiv=$('#helper');
+var tru = $('.true');
+var fals= $('.false');
+var z42 = $('.z42');
+var help =$('#forhelp');
+/*photo.hide();
+    textdiv.hide();
+    button.hide();*/
+$(button).click(function(){
+    photo.fadeOut(700);
+    textdiv.fadeOut(700);
+    button.fadeOut(700);
+});/*
+tru.click(function(){
+ //   textdiv.html('<p id="forhelp">   </p> ');
+ help.html('<h2 class="text"> Ви дали правильну відповідь  </h2> ');
+ photo.fadeIn(700);
+ textdiv.fadeIn(700);
+ button.fadeIn(700);
+});*/
+$('#dozt').click(function(){
+    textdiv.html('<p id="forhelp"> <span class="grenn" > Вірно</span> <br> Ви можете перейти на  <a class="go" href="rest.html" title="Артем.Дозвілля" target="_blanc">Артем.Дозвілля</a> . Там Ви знайдете , чим можна зайнятися , коли нічого робити</p>');
+    photo.fadeIn(700);
+    textdiv.fadeIn(700);
+    button.fadeIn(700);
+    percent_dovn++;
+});
+$('#dozf').click(function(){
+    textdiv.html('<p id="forhelp"> <span class="redd" > Невірно</span> <br>Артем.Дозвілля - один з сервісів нашог банку. Ви можете перейти на цю <a class="go" target="_blanc" title="Артем.Дозвілля" href="rest.html">Артем.Дозвілля</a> . Там Ви знайдете , чим можна зайнятися , коли нічого робити</p>');
+    photo.fadeIn(700);
+    textdiv.fadeIn(700);
+    button.fadeIn(700);
+    percent_dovn--;
+});
+$('#twof').click(function(){
+    textdiv.html('<p id="forhelp"> <span class="grenn" > Вірно</span> <br>А й справді,  якщо десять разів перемножити 2 на себе , то вйде не 1000, а 1024. А ось і посилання на Вікіпедію   <a class="go"  target="noname" href="https://uk.wikipedia.org/wiki/%D0%A1%D1%82%D0%B5%D0%BF%D1%96%D0%BD%D1%8C"> Вікіпедію</a>, де ви можете почитати про степінь </p>' );
+    photo.fadeIn(700);
+    textdiv.fadeIn(700);
+    button.fadeIn(700);
+    percent_dovn++;
+});
+$('#twot').click(function(){
+    textdiv.html('<p id="forhelp"> <span class="redd" > Невірно</span> <br>Адже якщо перемножити 2 на себе 10 разів, вийде 1024, а не 1000 А ось і посилання на Вікіпедію   <a class="go"  target="noname" href="https://uk.wikipedia.org/wiki/%D0%A1%D1%82%D0%B5%D0%BF%D1%96%D0%BD%D1%8C"> Вікіпедію</a>, де ви можете почитати про степінь </p>');
+    photo.fadeIn(700);
+    textdiv.fadeIn(700);
+    button.fadeIn(700);
+    percent_dovn--;
+});
+$('#pdff').click(function(){
+    textdiv.html('<p id="forhelp"> <span class="grenn" > Вірно</span> <br>Це розширення </p>' );
+    photo.fadeIn(700);
+    textdiv.fadeIn(700);
+    button.fadeIn(700);
+    percent_dovn++;
+});
+$('#pdft').click(function(){
+    textdiv.html('<p id="forhelp"> <span class="redd" > Невірно</span> <br>Це розширення, адже там стоїть крапка. </p>');
+    photo.fadeIn(700);
+    textdiv.fadeIn(700);
+    button.fadeIn(700);
+    percent_dovn--;
+});
+$('#hdf').click(function(){
+    textdiv.html('<p id="forhelp"> <span class="grenn" > Вірно</span> <br> Адже це по-суті більше схоже на жарт </p>' );
+    photo.fadeIn(700);
+    textdiv.fadeIn(700);
+    button.fadeIn(700);
+    percent_dovn++;
+});
+$('#hdt').click(function(){
+    textdiv.html('<p id="forhelp"> <span class="redd" > Невірно</span> <br>У жорсткого  диску немає ядрів, а блок живлення грає ключову роль, у компьютері, без нього  не можна запустити нічого </p>');
+    photo.fadeIn(700);
+    textdiv.fadeIn(700);
+    button.fadeIn(700);
+    percent_dovn--;
+});
+$('#zipt').click(function(){
+    textdiv.html('<p id="forhelp"> <span class="grenn" > Вірно</span> <br> Це досить старий вірус, про нього також написана стяття на порталі <a class="go" target="noname" title="Артем.Дозвілля" href="zip42.html">Артем.Дозвілля</a> . Там Ви знайдете про нього багато інформації , а також дещо інше</p>');
+    photo.fadeIn(700);
+    textdiv.fadeIn(700);
+    button.fadeIn(700);
+    percent_dovn++;
+});
+$('#zipf').click(function(){
+    textdiv.html('<p id="forhelp"> <span class="redd" > Невірно</span> <br>Це -таки вірус, про нього також написана стяття на порталі <a class="go" target="noname" title="Артем.Дозвілля" href="zip42.html">Артем.Дозвілля</a> . Там Ви знайдете про нього багато інформації , а також дещо інше</p>');
+    photo.fadeIn(700);
+    textdiv.fadeIn(700);
+    button.fadeIn(700);
+    percent_dovn--;
+
+});
+$('#actionn').click(function(){
+     if(percent_dovn == -5 ){
+        textdiv.html('<p id="forhelp"> <span class="redd" > Мало</span> <br>Ви набрали замало балів, а саме '+percent_dovn+ '</p>');
+        photo.fadeIn(700);
+        textdiv.fadeIn(700);
+        button.fadeIn(700);
+        $("#fix-5").fadeIn(1000).css("z-index",'20');
+
+
+    }
+    if(percent_dovn == -4 ){
+        textdiv.html('<p id="forhelp"> <span class="redd" > Мало</span> <br>Ви набрали замало балів, а саме '+percent_dovn+ '</p>');
+        photo.fadeIn(700);
+        textdiv.fadeIn(700);
+        button.fadeIn(700);
+        $("#fix-4").fadeIn(1000).css("z-index",'20');
+
+    }
+    if(percent_dovn == -3 ){
+        textdiv.html('<p id="forhelp"> <span class="redd" > Мало</span> <br>Ви набрали замало балів, а саме '+percent_dovn+ '</p>');
+        photo.fadeIn(700);
+        textdiv.fadeIn(700);
+        button.fadeIn(700);
+        $("#fix-3").fadeIn(1000).css("z-index",'20');
+
+    }
+    if(percent_dovn == -2 ){
+        textdiv.html('<p id="forhelp"> <span class="redd" > Мало</span> <br>Ви набрали замало балів, а саме '+percent_dovn+ '</p>');
+        photo.fadeIn(700);
+        textdiv.fadeIn(700);
+        button.fadeIn(700);
+        $("#fix-2").fadeIn(1000).css("z-index",'20');
+
+    }
+    if(percent_dovn == -1 ){
+        textdiv.html('<p id="forhelp"> <span class="redd" > Мало</span> <br>Ви набрали замало балів, а саме '+percent_dovn+ '</p>');
+        photo.fadeIn(700);
+        textdiv.fadeIn(700);
+        button.fadeIn(700);
+        $("#fix-1").fadeIn(1000).css("z-index",'20');
+
+    }
+    if(percent_dovn ==  0){
+        textdiv.html('<p id="forhelp"> <span class="redd" > Мало</span> <br>Ви набрали замало балів, а саме '+percent_dovn+ '</p>');
+        photo.fadeIn(700);
+        textdiv.fadeIn(700);
+        button.fadeIn(700);
+        $("#fix0").fadeIn(1000).css("z-index",'20');
+
+    }
+    if(percent_dovn == 1 ){
+        textdiv.html('<p id="forhelp"> <span class="redd" > Мало</span> <br>Ви набрали замало балів, а саме '+percent_dovn+ '</p>');
+        photo.fadeIn(700);
+        textdiv.fadeIn(700);
+        button.fadeIn(700);
+        $("#fix1").fadeIn(1000).css("z-index",'20');
+
+
+    }
+    if(percent_dovn == 2 ){
+        textdiv.html('<p id="forhelp"> <span class="redd" > Мало</span> <br>Ви набрали замало балів, а саме '+percent_dovn+ '</p>');
+        photo.fadeIn(700);
+        textdiv.fadeIn(700);
+        button.fadeIn(700);
+        $("#fix2").fadeIn(1000).css("z-index",'20');
+
+    }
+
+
+    if(percent_dovn==3 ){
+        textdiv.html('<p id="forhelp"> <span class="redd" > Малувато</span> <br>Ви набрали невелику кількість балів, а саме '+percent_dovn+ '</p>');
+        photo.fadeIn(700);
+        textdiv.fadeIn(700);
+        button.fadeIn(700);
+        $("#fix3").fadeIn(1000).css("z-index",'20');
+
+    }
+    if(percent_dovn==4){
+        textdiv.html('<p id="forhelp"> <span class="redd" > Багато</span> <br>Ви набрали багато балів, а саме '+percent_dovn+ '</p>');
+        photo.fadeIn(700);
+        textdiv.fadeIn(700);
+        button.fadeIn(700);
+        $("#fix4").fadeIn(1000).css("z-index",'20');
+
+    }
+    if(percent_dovn==5){
+        textdiv.html('<p id="forhelp"> <span class="redd" > Багато</span> <br>Ви набрали багато балів, а саме '+percent_dovn+ '</p>');
+        photo.fadeIn(700);
+        textdiv.fadeIn(700);
+        button.fadeIn(700);
+        $("#fix5").fadeIn(1000).css("z-index",'20');
+
+    }
+    
+});
 });
 
